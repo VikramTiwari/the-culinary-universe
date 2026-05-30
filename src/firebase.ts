bce6e4016417fda18c9a6cce9cebb0b452fa72d5
Vikram Tiwari <vikramtheone1@gmail.com>
2026-05-30 15:45:20 +0530

    chore: firebase creds cleanup

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyABF0TnrJ_LmkXzpgatFPETdDv_KC6clBI",
-  authDomain: "culinary-universe.firebaseapp.com",
-  projectId: "culinary-universe",
-  storageBucket: "culinary-universe.firebasestorage.app",
-  messagingSenderId: "533626372925",
-  appId: "1:533626372925:web:b8225bf4a1bda2a9673b54",
-  measurementId: "G-EYZTGCMCHL"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
